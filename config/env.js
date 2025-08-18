73169ff8061506316ae5095dcca6b6905edacfe0
loxxyN <emoyko@yandex.ru>
2025-08-18 23:22:09 +0700

    fix(env.js) fixed incorrect import in ENV object

--- config/env.js
+++ config/env.js
@@ -1,6 +1,6 @@
 const ENV = {
 	COINGECKO_API_KEY: import.meta.env.VITE_COINGECKO_API_KEY,
-	BASE_URL: import.meta.env.BASE_URL,
+	BASE_URL: import.meta.env.VITE_BASE_URL,
 }
 
 export const BASE_URL = ENV.BASE_URL
