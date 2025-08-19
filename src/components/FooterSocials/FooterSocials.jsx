67ddca7cc95032f59f035740723d63489e4ffa50
loxxyN <emoyko@yandex.ru>
2025-08-19 14:54:15 +0700

    feat(FooterSocials): Deleted LinkedIn icon and added social links to icons

--- src/components/FooterSocials/FooterSocials.jsx
+++ src/components/FooterSocials/FooterSocials.jsx
@@ -1,25 +1,23 @@
-import { FaDiscord, FaGithub, FaLinkedin, FaTelegram } from 'react-icons/fa'
+import { FaDiscord, FaGithub, FaTelegram } from 'react-icons/fa'
 
 export const FooterSocials = () => {
 	return (
 		<ul className='flex gap-x-4 justify-evenly items-center'>
 			<li>
-				<a href='#'>
+				<a href='https://t.me/L0xxyN' target='_blank'>
 					<FaTelegram className='text-gray-500 hover:text-gray-400 transition ' />
 				</a>
 			</li>
 			<li>
-				<a href='#'>
+				<a
+					href='https://discordapp.com/users/860042883246325781'
+					target='_blank'
+				>
 					<FaDiscord className='text-gray-500 hover:text-gray-400 transition' />
 				</a>
 			</li>
 			<li>
-				<a href='#'>
-					<FaLinkedin className='text-gray-500 hover:text-gray-400 transition' />
-				</a>
-			</li>
-			<li>
-				<a href='#'>
+				<a href='https://github.com/LoxxyN' target='_blank'>
 					<FaGithub className='text-gray-500 hover:text-gray-400 transition' />
 				</a>
 			</li>
